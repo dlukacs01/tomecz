b4a8a4c625f3557a25162c27a12bfd522c3b82fe
David Lukacs <lukacs.dvid@gmail.com>
2026-03-28 16:13:54 +0100

    breeze added

--- routes/web_old.php
+++ routes/web_old.php
@@ -1,27 +1,11 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// SYSTEM
-
-// phpinfo
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/phpinfo', function () { phpinfo(); });
-});
-
-// HOME
-
-/* Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-}); */
-
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
-Route::get('/kereses', [HomeController::class, 'search'])->name('home.search');
-Route::post('/locale', [HomeController::class, 'locale'])->name('home.locale');
-
-// ADMIN
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
